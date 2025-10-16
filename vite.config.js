2b983a5b7c77d9128b38d4cce13bdcea6906f662
Mahipat Makwana <86786837+MAFIA003@users.noreply.github.com>
2025-10-17 03:06:52 +0530

    update vite.config.js for deploy

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'git@github.com:MAFIA003/Hey-Web.git',
+  base: '/git@github.com:MAFIA003/Hey-Web.git/',
 })
